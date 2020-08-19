92b49a31708573134cef76d07b52ca914f484217
Michael Thomas <michaelhthomas@outlook.com>
2020-08-19 11:42:42 -0400

    First attempt with Reactduino

--- lovebox/lovebox.ino
+++ lovebox/lovebox.ino
@@ -1,8 +1,9 @@
+#include <Reactduino.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
 #include <EEPROM.h>
 #include <Servo.h>
-#include "SSD1306Wire.h"
+#include <SSD1306Wire.h>
 
 #include "settings.h"
 const int fetchIntervalMillis = _fetchIntervalSeconds * 1000;
@@ -12,7 +13,7 @@ const String url = _url;
 const int lightValueThreshold = _lightValueThreshold;
 
 SSD1306Wire oled(0x3C, D2, D1);
-Servo myservo; 
+Servo heartServo; 
 int pos = 90;
 int increment = -1;
 int lightValue;
@@ -20,29 +21,11 @@ String line;
 String mode;
 char idSaved = '0'; 
 bool wasRead = true;
+reaction process;
 
-void drawMessage(const String& message) {
-  Serial.print("Drawing message....");
-  oled.clear();
-
-  // Unterscheide zwischen Text und Bild
-  if(mode[0] == 't'){
-    oled.drawStringMaxWidth(0, 0, 128, message);    
-  } 
-  else {
-    for(int i = 0; i <= message.length(); i++){
-      int x = i % 129;
-      int y = i / 129;
-    
-      if(message[i] == '1'){
-        oled.setPixel(x, y);
-      }
-    } 
-  }    
-  oled.display();
-  Serial.println("done.");
-}
-
+/*
+ *  Make sure WiFi is connected and load credentials
+ */
 void wifiConnect() {
   Serial.printf("Connecting to WiFi '%s'...", ssid);
   if (WiFi.status() != WL_CONNECTED) {
@@ -58,6 +41,9 @@ void wifiConnect() {
   Serial.println(WiFi.localIP());
 }
 
+/*
+ *  Connect to the server and download message from gist
+ */
 void getGistMessage() {
   Serial.print("Fetching message...");
   const int httpsPort = 443;
@@ -71,7 +57,7 @@ void getGistMessage() {
     return; // Connection failed
   }
 
-  // current millis used to as a cache-busting means
+  // current millis used as a cache-busting means
   client.print(String("GET ") + url + "?" + millis() + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" +
                "User-Agent: ESP8266\r\n" +
@@ -102,8 +88,36 @@ void getGistMessage() {
   }
 }
 
+/*
+ *  Display message on screen
+ */
+void drawMessage(const String& message) {
+  Serial.print("Drawing message....");
+  oled.clear();
+
+  // Unterscheide zwischen Text und Bild
+  if(mode[0] == 't'){
+    oled.drawStringMaxWidth(0, 0, 128, message);    
+  } 
+  else {
+    for(int i = 0; i <= message.length(); i++){
+      int x = i % 129;
+      int y = i / 129;
+    
+      if(message[i] == '1'){
+        oled.setPixel(x, y);
+      }
+    } 
+  }    
+  oled.display();
+  Serial.println("done.");
+}
+
+/*
+ *  Spin the servo when a new message is available
+ */
 void spinServo() {
-  myservo.write(pos);      
+  heartServo.write(pos);      
   delay(50);    // wait 50ms to turn servo
 
   if(pos == 75 || pos == 105){ // 75°-105° range
@@ -112,32 +126,15 @@ void spinServo() {
   pos += increment;
 }
 
-void setup() {
-  // Setup serial
-  Serial.begin(9600);
-  Serial.println("\n\n");
-  
-  Serial.print("Attatching servo...");
-  myservo.attach(16);       // Servo on D0
-  Serial.println("done.");
-  
-  Serial.print("Initializing display...");
-  oled.init();
-  oled.flipScreenVertically();
-  oled.setColor(WHITE);
-  oled.setTextAlignment(TEXT_ALIGN_LEFT);
-  oled.setFont(ArialMT_Plain_10);
-     
-  oled.clear();
-  oled.drawString(30, 30, "<3 LOVEBOX <3");
-  oled.display();
-  Serial.println("done.");
-  
-  wifiConnect();
-
-  EEPROM.begin(512);
-  idSaved = EEPROM.get(142, idSaved);
-  wasRead = EEPROM.get(144, wasRead);
+/*
+ *  Turn screen on/off based on light value
+ */
+void checkScreen() {
+  if(lightValue > lightValueThreshold) {
+    oled.on();
+  } else {
+    oled.off();
+  }
 }
 
 void loop() {
@@ -159,6 +156,43 @@ void loop() {
       EEPROM.commit();
     }
   }
-  
-  delay(fetchIntervalMillis);
 }
+
+Reactduino app([] () {
+  // Setup serial
+  Serial.begin(9600);
+  Serial.println("\n\n");
+  
+  Serial.print("Attatching servo...");
+  heartServo.attach(16);       // Servo on D0
+  Serial.println("done.");
+  
+  Serial.print("Initializing display...");
+  oled.init();
+  oled.flipScreenVertically();
+  oled.setColor(WHITE);
+  oled.setTextAlignment(TEXT_ALIGN_LEFT);
+  oled.setFont(ArialMT_Plain_10);
+     
+  oled.clear();
+  oled.drawString(30, 30, "<3 LOVEBOX <3");
+  oled.display();
+  Serial.println("done.");
+  
+  wifiConnect();
+
+  EEPROM.begin(512);
+  idSaved = EEPROM.get(142, idSaved);
+  wasRead = EEPROM.get(144, wasRead);
+
+  // Turn screen on and off based on light value
+  app.repeat(100, checkScreen);
+
+  if wasRead {
+    // Check for and display new messages
+    process = app.repeat(fetchIntervalMillis, getGistMessage);
+  } else {
+    // Wait for message to be read
+    process = app.repeat(50, spinServo);
+  }
+});
